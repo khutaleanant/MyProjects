09a4035609c2c20859ede31b4b827acb433b5b36
Anant Khutale <khutaleanant@gmail.com>
2025-03-27 16:00:38 +0530

    uptodate

--- DataSciencePracticeFiles/EDA - Range and Validity of Data.py
+++ DataSciencePracticeFiles/EDA - Range and Validity of Data.py
@@ -1,5 +1,38 @@
 # Identification:- Check if numerical data values fall within expected ranges
 
+# Example 1
+
 import pandas as pd
 
 DataFrame=pd.read_csv("C:\Data Science Programs Practice\Salary DataSet.csv")
+
+# For salary, it should be greater than 50000
+
+Minimum_Salary=50000
+
+Salary_Outliers=DataFrame[DataFrame["Salary"]<Minimum_Salary]
+
+# Displaying the Outlier in the Salary column
+
+print(Salary_Outliers)
+
+##################################################3
+
+# Example 2
+
+DataFrame2=pd.read_csv("C:\Data Science Programs Practice\Mall_Customers.csv")
+
+# For Age, it should be greater than 20
+
+Minimum_Age=20
+
+Age_Outliers=DataFrame2[DataFrame2["Age"]<Minimum_Age]
+
+# Displaying the Outlier in the Salary column
+
+print(Age_Outliers)
+
+
+
+
+
